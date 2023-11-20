3331c1df05c47856172cfd76c34c140e1d382616
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-11-20 15:06:25 +0200

    add

---
+++ script/code.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('[data-submit]')
+
+function countdown (){
+    let input1 = document.querySelector('[type="text"]').value
+    let input2 = document.querySelector('[type="date"]').value
+    let output = document.querySelector('[class="output"]')
+    if ()
+    let today = new Date()
+    let bday = 
+}
+
+btn.addEventListener('click', countdown)
